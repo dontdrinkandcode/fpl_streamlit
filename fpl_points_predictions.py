1c26bd8080a1c2ad71a6609e6652913643485c1d
Craig Armstrong <c.t.armstrong85@gmail.com>
2023-12-04 16:12:39 +0000

    GW15

--- fpl_points_predictions.py
+++ fpl_points_predictions.py
@@ -15,7 +15,7 @@ df = read_data()
 
 st.title('FPL points predictor')
 
-st.sidebar.title("GW 12")
+st.sidebar.title("GW 15")
 
 st.write('A free and straightforward tool for helping with your FPL team selection.')
 
